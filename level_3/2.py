4fefec66afdd73f4b6a8cb9d382c71cc29572890
Alexander P <alexander@MacBook-Air-Aleksandr.local>
2024-03-29 16:11:26 +0300

    level 3

--- level_3/2.py
+++ level_3/2.py
@@ -1,7 +1,13 @@
 from constants import ___
+from typing import TypedDict
 
 
-def calculate_total_spent_for_user(user: ___) -> ___:
+class User(TypedDict):
+    name: str
+    age: int
+    transactions_sums: list[int]
+
+def calculate_total_spent_for_user(user: User) -> int:
     # попробуй тут воспользовать typing.TypedDict
     pass
 
